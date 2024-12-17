2cf7588a433a62660ffa7dd52567862c53b31b42
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-17 06:55:14 +0000

    Initial commit

---
+++ .lesson/instructions.md
@@ -0,0 +1,3 @@
+# Instructions  
+
+Write a program that will ask the user to enter the amount of a purchase. The program should then compute the state and county sales tax. Assume the state sales tax is 5 percent and the county sales tax is 3 percent. The program should display the amount of the purchase, the state sales tax, the county sales tax, the total sales tax, and the total of the sale (which is the sum of the amount of purchase plus the total sales tax). Hint: Use the value 0.03 to represent 3 percent, and 0.05 to represent 5 percent.
---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner; // Needed for the Scanner class
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      // Constants
+      
+      // Variables
+      
+      // Create a Scanner object for keyboard input.
+             
+      // Get the amount of the purchase.
+      
+      // Calculate the state sales tax.
+      
+      // Calculate county sales tax.
+      
+      // Calculate the total sales tax.
+      
+      // Calculate the total purchase cost.
+      
+      // Display the results.
+   }
+}
